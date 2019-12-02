e07037b4588e96be1205b6fcc60f9e4a40de20ab
Alexander Hugo <alexander.ti.info@gmail.com>
2019-12-02 08:56:58 -0300

    Update btree.py

--- btree.py
+++ btree.py
@@ -1,9 +1,4 @@
 # coding=utf-8
-"""
-author = Mateor
-PYTHON 3.3.5
-"""
-
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
                         print_function, unicode_literals)
 import pandas as pd
